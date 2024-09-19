d5019aa0c06ccdfd96b53f31d6aeb172542316d8
PabloNotEscobar <0kvestik0@gmail.com>
2024-09-19 15:52:41 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css'
+
+const tg = window.Telegram.WebApp;
+
+function App() {
+
+    const onClose = () => {
+        tg.close()
+    }
+
+  return (
+    <div className="App">
+      work
+        <button onClick={onClose}>Закрыть</button>
+    </div>
+  );
+}
+
+export default App;
